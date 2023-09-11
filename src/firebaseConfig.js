2e42da1583407be360225c01dda6db71c46288ff
luongthao2711 <114208722+luongthao2711@users.noreply.github.com>
2023-09-11 11:43:14 +0700

    add priject reactjs66

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDeFKz57W4YwVlcQEGtv33Vknp3trb9gqw",
-  authDomain: "vtireactjs-ed1c9.firebaseapp.com",
-  projectId: "vtireactjs-ed1c9",
-  storageBucket: "vtireactjs-ed1c9.appspot.com",
-  messagingSenderId: "586080499297",
-  appId: "1:586080499297:web:04726d21f5461cb4b5dc20",
-  measurementId: "G-GX53LFH836",
+  apiKey: "AIzaSyDuejVK6hsJ7BIWVJbc9Ne_8dP1Q7MkBUM",
+  authDomain: "reactjsvtira66.firebaseapp.com",
+  projectId: "reactjsvtira66",
+  storageBucket: "reactjsvtira66.appspot.com",
+  messagingSenderId: "193734746323",
+  appId: "1:193734746323:web:0968d5a6d95e19c5a0f426",
+  measurementId: "G-MF159HRRWL",
 };
 
 //* Initialize Firebase
