4be5c2b51ede21b05bbf26d9389d00c1b298c784
luongthao2711 <114208722+luongthao2711@users.noreply.github.com>
2023-09-11 14:13:29 +0700

    add project react

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,24 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDuejVK6hsJ7BIWVJbc9Ne_8dP1Q7MkBUM",
-  authDomain: "reactjsvtira66.firebaseapp.com",
-  projectId: "reactjsvtira66",
-  storageBucket: "reactjsvtira66.appspot.com",
-  messagingSenderId: "193734746323",
-  appId: "1:193734746323:web:0968d5a6d95e19c5a0f426",
-  measurementId: "G-MF159HRRWL",
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCJaFPtqQon4IpURSEVLBTAgJimgFZHRQ0",
+//   authDomain: "reactjsra66.firebaseapp.com",
+//   projectId: "reactjsra66",
+//   storageBucket: "reactjsra66.appspot.com",
+//   messagingSenderId: "370269683106",
+//   appId: "1:370269683106:web:bcb28ee27d36a62038c6fb",
+//   measurementId: "G-T2ENMHCB2L",
+// };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// //* Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const database = getAuth(app);
-
-const analytics = getAnalytics(app);
+// //* Initialize Firebase Authentication and get a reference to the service
+// export const database = getAuth(app);
+// export const analytics = getAnalytics(app);
